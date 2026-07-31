8ac3544f563f7523fdf6e835e0bfc04c6f6a8206
gtkim1000 <gtkim1000@naver.com>
2026-07-31 11:38:21 +0900

    Prepare VIZENTIVE site for deployment

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "비젠티브 | VIZENTIVE",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
